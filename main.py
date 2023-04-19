14d055774b930af94acb12781d990fc37c9887b1
ilya-obi <ilya.obyedkov2014@yandex.ru>
2023-04-19 14:17:41 +0300

    Broke down to functions

--- main.py
+++ main.py
@@ -2,50 +2,40 @@
 # heroes = 'heroes = (("Илья М.", (1, 2, 3)), ("Алёша П.", (1, )), ("Добрыня Н.", (2, 3)))'
 # mission = 'mission = (1,1,2)'
 
-heroes = 'heroes = (("Илья М.", (1, 2, 3)),)'
-mission = 'mission = (1,)'
+# heroes = 'heroes = (("Илья М.", (1, 2, 3)),)'
+# mission = 'mission = (1,)'
 
-# heroes = 'heroes = (("Илья М.", (1, 2, 3)), ("Алёша П.", (1, )),)'
-# mission = 'mission = (1, 2)'
+heroes = 'heroes = (("Илья М.", (1, 2, 3)), ("Алёша П.", (1, )),)'
+mission = 'mission = (1, 2)'
 
 skills = []
 names = []
-skills_req = []
+skills_reqiredired = []
 
 
 def distribute():
-    skills_req = mission[mission.index("(")+1:mission.rindex(")")]
-    skills_req = skills_req.split(",")
-    for i in range(len(skills_req)):
-        skills_req[i] = skills_req[i].replace(" ", "")
-        skills_req[i] = skills_req[i].replace(")", "")
-        skills_req[i] = skills_req[i].replace(",", "")
-        if (skills_req[i]) == "":
-            skills_req.pop(i)
-        # print(skills_req[i])
+    skills_reqired = mission[mission.index("(")+1:mission.rindex(")")]
+    skills_reqired = skills_reqired.split(",")
+    parse_mission(skills_reqired, mission)
     parse_heroes_skills(heroes, names, skills)
     counter = [0]*len(skills)
-    # for skill in skills_req:
-    #     print(skill)
-    # for skill in skills:
-    #     print(skill)
-    # for name in names:
-    #     print(name)
-    print(names)
-    print(skills_req)
-    print(skills)
-    for i, skill_r in enumerate(skills_req):
-        for skill in skills:
-            if skill_r in skill:
-                counter[i] += 1
+    skill_counter(counter, skills_reqired)
+
+    # counter = [0]*len(skills)
+    # for i, skill_r in enumerate(skills_reqired):
+    #     for skill in skills:
+    #         if skill_r in skill:
+    #             counter[i] += 1
     print(counter)
+    print(skills_reqired)
+    print(skills)
+    print(names)
 
 
 def parse_heroes_skills(heroes, names, skills):
     heroes = heroes[8:]
     heroes_cut = heroes.split("),")
     for h in heroes_cut:
-        # print(h)
         try:
             names.append(h[h.index('"')+1: h.rindex('"')])
         except Exception:
@@ -54,6 +44,23 @@ def parse_heroes_skills(heroes, names, skills):
             skills.append(h[h.index(",")+3:h.rindex(")")])
         except Exception:
             pass
+
+
+def parse_mission(skills_reqired, mission):
+    for i in range(len(skills_reqired)):
+        skills_reqired[i] = skills_reqired[i].replace(" ", "")
+        skills_reqired[i] = skills_reqired[i].replace(")", "")
+        skills_reqired[i] = skills_reqired[i].replace(",", "")
+        if (skills_reqired[i]) == "":
+            skills_reqired.pop(i)
+
+
+def skill_counter(counter, skills_reqired):
+
+    for i, skill_r in enumerate(skills_reqired):
+        for skill in skills:
+            if skill_r in skill:
+                counter[i] += 1
 
 
 distribute()
