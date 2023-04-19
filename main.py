8fad83e1f34f026fd4bd72622948d30e79f144a9
ilya-obi <ilya.obyedkov2014@yandex.ru>
2023-04-19 16:21:06 +0300

    Holy cow. I think it works?!

--- main.py
+++ main.py
@@ -5,8 +5,11 @@
 # heroes = 'heroes = (("Илья М.", (1, 2, 3)),)'
 # mission = 'mission = (1,)'
 
+# heroes = 'heroes = (("Илья М.", (1, 2, 3)), ("Алёша П.", (1, )),)'
+# mission = 'mission = (1, 2)'
+
 heroes = 'heroes = (("Илья М.", (1, 2, 3)), ("Алёша П.", (1, )),)'
-mission = 'mission = (1, 2)'
+mission = 'mission = (1, 5)'
 
 
 def distribute():
@@ -19,7 +22,13 @@ def distribute():
     parse_mission(skills_reqired, mission)
     counter = [0]*len(skills)
     skill_counter(counter, skills_reqired, skills)
-    logger(counter, skills_reqired, skills, names)
+    counter, skills_reqired = zip(
+        *sorted(zip(counter, skills_reqired)))
+    skills, names = zip(
+        *sorted(zip(skills, names), key=lambda x: len(x[0])))
+    # logger(counter, skills_reqired, skills, names)
+    answer_writer(counter, skills_reqired, skills, names)
+    # return (answer_writer(counter, skills_reqired, skills, names))
 
 
 def parse_heroes_skills(heroes, names, skills):
@@ -34,6 +43,7 @@ def parse_heroes_skills(heroes, names, skills):
             skills.append(h[h.index(",")+3:h.rindex(")")])
         except Exception:
             pass
+    skills[len(skills)-1] = skills[len(skills)-1].replace("))", "")
 
 
 def parse_mission(skills_reqired, mission):
@@ -58,6 +68,30 @@ def logger(counter, skills_reqired, skills, names):
     print(skills_reqired)
     print(skills)
     print(names)
+
+
+def answer_writer(counter, skills_reqired, skills, names):
+    answ = []
+    names_copy = list(names)
+    skills_reqired_copy = list(skills_reqired)
+    skills_copy = list(skills_reqired)
+    if counter[0] == 0:
+        print([])
+        return answ
+    else:
+        for skill_req in skills_reqired_copy:
+            for index, skill in enumerate(skills_copy):
+                if skill_req in skill:
+                    try:
+                        answ.append(names_copy.pop(index))
+                        skills_copy.pop(index)
+                    except Exception:
+                        pass
+    if names_copy == []:
+        print(answ)
+        return answ
+    else:
+        return []
 
 
 distribute()
